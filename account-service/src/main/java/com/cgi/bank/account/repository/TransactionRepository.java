5808eb158ccab04601508cf9417d6c3cd7137008
Jaak Vaher <jaak.vaher@gmail.com>
2025-04-04 12:17:13 +0300

    removed unused import and comment

--- account-service/src/main/java/com/cgi/bank/account/repository/TransactionRepository.java
+++ account-service/src/main/java/com/cgi/bank/account/repository/TransactionRepository.java
@@ -1,8 +1,5 @@
 package com.cgi.bank.account.repository;
 
-import java.time.Instant;
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
